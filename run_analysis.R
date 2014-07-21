1153823bc5804dde9f14ada48561195b3688ec55
dcadoff <dave.cadoff@gmail.com>
2014-07-21 12:08:50 -0700

    Script with comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,93 +1,88 @@
-## You should create one R script called run_analysis.R that does the following. 
-## Merges the training and the test sets to create one data set.
-## Extracts only the measurements on the mean and standard deviation for each measurement. 
-## Uses descriptive activity names to name the activities in the data set
-## Appropriately labels the data set with descriptive variable names. 
-## Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+## This script does the following:
+## 1) Merges the training and the test sets to create one data set.
+## 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+## 3) Uses descriptive activity names to name the activities in the data set
+## 4) Appropriately labels the data set with descriptive variable names. 
+## 5) Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
-## Script and data need to be in the same working directory to run properly
+## WARNING: Script and data need to be in the same working directory to run properly!  Data needs to
+## retain the same sub-directories, too.
 
 ## Load raw data into data tables
-xtrainrawdata <- read.table("./train/X_train.txt")
-ytrainrawdata <- read.table("./train/y_train.txt")
-xtestrawdata <- read.table("./test/X_test.txt")
-ytestrawdata <- read.table("./test/y_test.txt")
+        xtrainrawdata <- read.table("./train/X_train.txt")
+        ytrainrawdata <- read.table("./train/y_train.txt")
+        xtestrawdata <- read.table("./test/X_test.txt")
+        ytestrawdata <- read.table("./test/y_test.txt")
 
-columnnames <- read.table("features.txt")
-activities <- read.table("activity_labels.txt")
+## Load column variable names and activity labels
+        columnnames <- read.table("features.txt")
+        activities <- read.table("activity_labels.txt")
 
-trainsubjects <- read.table("./train/subject_train.txt")
-testsubjects <- read.table("./test/subject_test.txt")
+## Load subject IDs
+        trainsubjects <- read.table("./train/subject_train.txt")
+        testsubjects <- read.table("./test/subject_test.txt")
 
 ## Add column names to raw data tables
-names(xtrainrawdata) <- columnnames[,2]
-names(xtestrawdata) <- columnnames[,2]
+        names(xtrainrawdata) <- columnnames[,2]
+        names(xtestrawdata) <- columnnames[,2]
 
-#combinetestsubjectandactivity <- cbind(testsubjects, ytestrawdata)
-#combinetrainsubjectandactivity <- cbind(trainsubjects, ytrainrawdata)
+## Combine training and test data sets
+        combinerawdata <- rbind(xtrainrawdata, xtestrawdata)
 
-#alltraindata <- cbind(combinetrainsubjectandactivity, xtrainrawdata)
-#alltestdata <- cbind(combinetestsubjectandactivity, xtestrawdata)
+## Subset by Mean and Standard Deviation
+        subsetdata <- grep("[sS]td|-[mM]ean", colnames(combinerawdata))
+        cleandata <- combinerawdata[,subsetdata]
 
-#colnames(alltraindata)[1] <- "Subject"
-#colnames(alltraindata)[2] <- "Activity Type"
-#colnames(alltestdata)[1] <- "Subject"
-#colnames(alltestdata)[2] <- "Activity Type"
+## Drop meanFreq from remaining data; Mean Frequency is a weighted average not a standard mean
+        subsetdata <- grep("Freq", colnames(cleandata), invert = TRUE)
+        cleandata <- cleandata[,subsetdata]
 
-combinerawdata <- rbind(xtrainrawdata, xtestrawdata)
+## Create subject and activity data set
+        combinetestsubjectandactivity <- cbind(testsubjects, ytestrawdata)
+        combinetrainsubjectandactivity <- cbind(trainsubjects, ytrainrawdata)
+        subjectactivity <- rbind(combinetrainsubjectandactivity, combinetestsubjectandactivity)
 
-#Subset by Mean and Standard Deviation
-subsetdata <- grep("[sS]td|-[mM]ean", colnames(combinerawdata))
-cleandata <- combinerawdata[,subsetdata]
+## Add column names for subject and activity       
+        colnames(subjectactivity)[1] <- "Subject"
+        colnames(subjectactivity)[2] <- "ActivityType"
 
-#Drop meanFreq from remaining data; Mean Frequency is a weighted average not a standard mean
-subsetdata <- grep("Freq", colnames(cleandata), invert = TRUE)
-cleandata <- cleandata[,subsetdata]
+## Creat tidy variable names
+        colnames2 <- colnames(cleandata)
+        colnames2 <- sub("-X", "Xaxis", colnames2)
+        colnames2 <- sub("-Y", "Yaxis", colnames2)
+        colnames2 <- sub("-Z", "Zaxis", colnames2)
+        colnames2 <- sub("\\(\\)", "", colnames2)
+        colnames2 <- sub("-mean()", "Mean", colnames2)
+        colnames2 <- sub("-std", "StandardDeviation", colnames2)
+        colnames2 <- sub("^f", "FrequencySignal", colnames2)
+        colnames2 <- sub("^t", "TimeSignal", colnames2)
+        colnames2 <- sub("Acc", "Acceleration", colnames2)
+        colnames2 <- sub("Mag", "Magnitude", colnames2)
+        colnames2 <- sub("Gyro", "Gyroscope", colnames2)
 
+## Add tidy variable names to cleaned data set
+        names(cleandata) <- colnames2
 
-combinetestsubjectandactivity <- cbind(testsubjects, ytestrawdata)
-combinetrainsubjectandactivity <- cbind(trainsubjects, ytrainrawdata)
+## Combine subject, activity and cleaned data set
+        finalrawdata <- cbind(subjectactivity, cleandata)
 
-subjectactivity <- rbind(combinetrainsubjectandactivity, combinetestsubjectandactivity)
-colnames(subjectactivity)[1] <- "Subject"
-colnames(subjectactivity)[2] <- "ActivityType"
+## Merge activity labels with combined data set
+## Note: merging adds an extra column that needs to be deleted; Also, changes the order of the columns
+## which needs to be restored. Finally, Activity column name needs to be restored.
+        
+        finalrawdata <- merge(activities, finalrawdata, by.y = "ActivityType", by.x = "V1")
+        finalrawdata$V1 <- NULL
+        finalrawdata <- finalrawdata[c(2,1,3:68)]
+        colnames(finalrawdata)[2] <- "ActivityType"
 
-## Make tidy variable names
-colnames2 <- colnames(cleandata)
-colnames2 <- sub("-X", "Xaxis", colnames2)
-colnames2 <- sub("-Y", "Yaxis", colnames2)
-colnames2 <- sub("-Z", "Zaxis", colnames2)
-colnames2 <- sub("\\(\\)", "", colnames2)
-colnames2 <- sub("-mean()", "Mean", colnames2)
-colnames2 <- sub("-std", "StandardDeviation", colnames2)
-colnames2 <- sub("^f", "FrequencySignal", colnames2)
-colnames2 <- sub("^t", "TimeSignal", colnames2)
-colnames2 <- sub("Acc", "Acceleration", colnames2)
-colnames2 <- sub("Mag", "Magnitude", colnames2)
-colnames2 <- sub("Gyro", "Gyroscope", colnames2)
+#Sort data set by Subject then by Activity Type
+        sorteddata <- finalrawdata[with(finalrawdata, order(finalrawdata[,1], finalrawdata[,2])), ]
 
-names(cleandata) <- colnames2
+## Aggregate data set by Subject and Activity.  Find mean of each aggregated subset.
+        aggregatemean <- aggregate(sorteddata[, 3:68], by = list(sorteddata$Subject, sorteddata$ActivityType), FUN = mean)
+        colnames(aggregatemean)[1:2] <- c("Subject", "Activity")
 
-finalrawdata <- cbind(subjectactivity, cleandata)
-
-
-
-finalrawdata <- merge(activities, finalrawdata, by.y = "ActivityType", by.x = "V1")
-
-finalrawdata$V1 <- NULL
-colnames(finalrawdata)[2] <- "ActivityType"
-
-
-#names(finalrawdata) <- colnames2
-
-
-#Sort by Subject then by Activity Type
-sorteddata <- finalrawdata[with(finalrawdata, order(finalrawdata[,1], finalrawdata[,2])), ]
-
-
-aggregatemean <- aggregate(sorteddata[, 3:68], by = list(sorteddata$Subject, sorteddata$ActivityType), FUN = mean)
-colnames(aggregatemean)[1:2] <- c("Subject", "Activity")
-
-write.table(aggregatemean, "tidy_data.txt", row.names = FALSE)
+## Write tidy data set to text file
+        write.table(aggregatemean, "tidy_data.txt", row.names = FALSE)
 
 
